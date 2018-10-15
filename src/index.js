1dae0bba3ba2f371a9c1039aa0527f096e2b10e8
Alvin <alvinsmtih.nz@gmail.com>
2018-10-15 22:14:30 +1300

    Add: Store and draw bus in part dynamically

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { Store } from './store';
 import './index.css';
 
 /* Creating a grid as container */
@@ -14,24 +15,38 @@ const generateParkContainer = (el) => {
         for (k = 0; k < 5; k += 1) {
             let box = document.createElement("div");
             box.className = "box";
-            box.id = "box" + k;
-            //box.innerHTML = k + ' ' + (5 - 1 - i);
-            // box.innerHTML = row.id + ' ' + box.id;
-            
-            // Place Bus
-            let img = document.createElement("i");
-            img.className = "fa fa-bus inner" + " ";
-            img.className += "rotate-west";
-            box.appendChild(img);
-            
-            // TODO: separate render and state
+            box.id = "Coordinate" + k + ',' + (5 - 1 - i); // Make id match the coordinate
+            // Add to row
             row.appendChild(box);
         }
-
+        // Add to the park container
         container.appendChild(row);
     }
-
+    // Add to the parent node 'main'. Could be document.body as well.
     el.appendChild(container);
 };
 
+// TODO: move to utils
+const placeBus = () => {
+    const buses = Store.getState().buses;
+    buses[0] = {
+        posX: 3,
+        posY: 2,
+        direction: 'NORTH',
+        id: '1'
+    };
+}; // TODO: END
+
+export const renderBusInPark = () => {
+    const buses = Store.getState().buses;
+    buses.map((bus) => {
+        const box = document.getElementById("Coordinate" + bus.posX + "," + bus.posY);
+        let img = document.createElement("i");
+        img.className = `fa fa-bus inner rotate-${(bus.direction).toLowerCase()}`;
+        box.appendChild(img);
+    });
+};
+
 generateParkContainer(document.getElementById('main'));
+placeBus();
+renderBusInPark();
---
+++ src/store/index.js
@@ -0,0 +1,32 @@
+const INITIAL_STATE = {
+    buses: [
+        {posX:0,posY:0,direction:'NORTH',id:'1'},
+        {posX:2,posY:1,direction:'WEST',id:'2'},
+        {posX:4,posY:4,direction:'EAST',id:'3'},
+        {posX:1,posY:1,direction:'SOUTH',id:'4'}
+    ],
+    selectedBusId: '',
+    parkSize: 5,
+    notification: '',
+    report: '',
+};
+
+export const Store = (() => {
+    // private variables
+    let state;
+    // private functions
+    const init = (data) => state = Object.assign({}, data);
+    const getAllState = () => state;
+
+    //public functions
+    return {
+        getState: function () {
+            return getAllState();
+        },
+        setState: function (data) {
+            return init(data);
+        }
+    };
+})();
+
+Store.setState(INITIAL_STATE);
